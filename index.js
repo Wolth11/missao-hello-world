f62d8b9e0c0028f5b6108c07f903ce60483a4683
Wolth11 <139875854+Wolth11@users.noreply.github.com>
2024-05-07 23:07:27 +0000

    nodemon instalado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("HELLO WORLD");
+    res.send("HELLO WORLD!");
 });
 
 app.get("/oi", function (req, res) {
