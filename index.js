6b9bf36b0ba87c115e1ff5c394196fa2553d8d98
Wolth11 <139875854+Wolth11@users.noreply.github.com>
2024-05-08 03:01:28 +0000

    mongodb update

--- index.js
+++ index.js
@@ -1,84 +1,100 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const lista = ["JAVA", "KOTLIN", "ANDROID"]
+const dbUrl = "mongodb+srv://admin:MhEfvW3C.wcTanK@cluster0.lygekv7.mongodb.net"
+const dbName = "mongodb-intro-e-imprementacao"
 
-app.get("/", function (req, res) {
-    res.send("HELLO WORLD!");
-});
+async function main() {
 
-app.get("/oi", function (req, res) {
-    res.send("olá mundo")
-})
+    const client = new MongoClient(dbUrl)
+    console.log("Conectando ao banco de dados...")
+    await client.connect()
+    console.log("Banco de dados conectado com sucesso!")
 
-app.get("/personagem", function (req, res) {
-    res.status(201).send(lista.filter(Boolean))
-})
+    const db = client.db(dbName)
+    const collection = db.collection("personagem")
 
-app.get("/personagem/:id", function (req, res) {
-    const id = req.params.id
-    const item = lista[id - 1]
+    app.get("/", function (req, res) {
+        res.send("HELLO WORLD!");
+    });
 
-    if (!item) {
-        return res.status(404).send("Item não encontrado.")
-    }
+    app.get("/oi", function (req, res) {
+        res.send("olá mundo")
+    })
 
-    res.status(201).send(item)
-})
+    app.get("/personagem", function (req, res) {
+        res.status(201).send(lista.filter(Boolean))
+    })
 
-app.use(express.json())
+    app.get("/personagem/:id", function (req, res) {
+        const id = req.params.id
+        const item = lista[id - 1]
 
-app.post("/personagem", function (req, res) {
-    const body = req.body
+        if (!item) {
+            return res.status(404).send("Item não encontrado.")
+        }
 
-    const novoItem = body.nome
+        res.status(201).send(item)
+    })
 
-    if (!novoItem) {
-        return res.status(400).send("Cadê o nome parsa?")
-    }
+    app.use(express.json())
 
-    if (lista.includes(novoItem)) {
-        return res.status(409).send("Você é a dory?")
-    }
+    app.post("/personagem", function (req, res) {
+        const body = req.body
 
-    lista.push(novoItem)
+        const novoItem = body.nome
 
-    res.status(201).send("item enviado com sucesso: " + novoItem)
+        if (!novoItem) {
+            return res.status(400).send("Cadê o nome parsa?")
+        }
 
-})
+        if (lista.includes(novoItem)) {
+            return res.status(409).send("Você é a dory?")
+        }
 
-app.put("/personagem/:id", function (req, res) {
-    const id = req.params.id
+        lista.push(novoItem)
 
-    if (!lista[id - 1]) {
-        return res.status(404).send("Item não encontrado.")
-    }
+        res.status(201).send("item enviado com sucesso: " + novoItem)
 
-    const body = req.body
-    const novoItem = body.nome
+    })
 
-    if (!novoItem) {
-        return res.status(400).send("Cadê o nome parsa?")
-    }
+    app.put("/personagem/:id", function (req, res) {
+        const id = req.params.id
 
-    if (lista.includes(novoItem)) {
-        return res.status(409).send("Você é a dory?")
-    }
+        if (!lista[id - 1]) {
+            return res.status(404).send("Item não encontrado.")
+        }
 
-    lista[id - 1] = novoItem
-    res.send("item atualizado com sucesso: " + id + " - " + novoItem)
-})
+        const body = req.body
+        const novoItem = body.nome
 
-app.delete("/personagem/:id", function (req, res) {
-    const id = req.params.id
+        if (!novoItem) {
+            return res.status(400).send("Cadê o nome parsa?")
+        }
 
-    if (!lista[id - 1]) {
-        return res.status(404).send("Item não encontrado.")
-    }
+        if (lista.includes(novoItem)) {
+            return res.status(409).send("Você é a dory?")
+        }
+
+        lista[id - 1] = novoItem
+        res.send("item atualizado com sucesso: " + id + " - " + novoItem)
+    })
+
+    app.delete("/personagem/:id", function (req, res) {
+        const id = req.params.id
+
+        if (!lista[id - 1]) {
+            return res.status(404).send("Item não encontrado.")
+        }
 
 
-    delete lista[id - 1]
+        delete lista[id - 1]
 
-    
-    res.send("Item removido com sucesso: " + id)
-})
-app.listen(3000)
+
+        res.send("Item removido com sucesso: " + id)
+    })
+    app.listen(3000)
+}
+
+main()
